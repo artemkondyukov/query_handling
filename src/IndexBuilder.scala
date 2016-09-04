5048b48c7ea701d0aca1737130dba60bd94ae38e
Artem Kondyukov <artemkondyukov@gmail.com>
2016-09-04 15:46:03 +0300

    Several documents support

--- src/IndexBuilder.scala
+++ src/IndexBuilder.scala
@@ -10,24 +10,47 @@ object IndexBuilder {
     string.replaceAll("[^A-Za-z0-9 -]", "").toLowerCase
   }
 
-  def buildInvertedIndex(corpusFilenames: List[String]): Map[String, (Int, String)] = {
-    val documentMap: Map[String, String] =
+  def buildInvertedIndex(corpusFilenames: List[String]) = { //: Map[String, List[(Int, String)]] = {
+    def mergeMaps[A, B](mergeFunction: (B, B) => B)(leftMap: Map[A, B], rightMap: Map[A, B]): Map[A, B] = {
+      leftMap ++
+        rightMap.filter(rTuple => !leftMap.contains(rTuple._1)) ++
+        rightMap.filter(rTuple => leftMap.contains(rTuple._1))
+        .map(rTuple => rTuple._1 -> mergeFunction(leftMap(rTuple._1), rTuple._2))
+    }
+    def mergeMapsInner = {
+      mergeMaps((lTuple: List[(Int, String)], rTuple: List[(Int, String)]) =>
+        List((lTuple.head._1 + rTuple.head._1, rTuple.head._2))) (
+          _: Map[String, List[(Int, String)]],
+          _: Map[String, List[(Int, String)]]
+      )
+    }
+
+    def mergeMapsOuter = {
+      mergeMaps((leftList: List[(Int, String)], rightList: List[(Int, String)]) =>
+        leftList ::: rightList) (
+          _: Map[String, List[(Int, String)]],
+          _: Map[String, List[(Int, String)]]
+      )
+    }
+
+    val documentMap =
       corpusFilenames.map(filename =>
         filename -> cleanString(scala.io.Source.fromFile(filename).mkString)).toMap
     documentMap.map { case (documentName, text) =>
       text.split(" ")
-        .map(word => Map(word -> (1, documentName)))
-        .reduce((leftMap, rightMap) =>
-          leftMap ++ rightMap.filter(rTuple => !leftMap.contains(rTuple._1))
-                  ++ rightMap.filter(rTuple => leftMap.contains(rTuple._1))
-          .map(rTuple => rTuple._1 -> (leftMap(rTuple._1)._1 + rTuple._2._1, rTuple._2._2))
+        .map(word => Map(word -> List((1, documentName))))
+        .reduce(
+          mergeMapsInner
         )
-    } head
+    }
+    .reduce(
+      mergeMapsOuter
+    )
   }
 
-  def getStopWords(index: Map[String, (Int, String)], num: Int) = {
-    scala.util.Sorting.stableSort(index.toList,
-      (left: (String, (Int, String)), right: (String, (Int, String))) =>
-        left._2._1 > right._2._1).take(10)
+  def getStopWords(index: Map[String, List[(Int, String)]], num: Int) = {
+    val sumCounts: Map[String, Int] = index.mapValues(list => list.foldLeft(0)((sum, rTuple) => sum + rTuple._1))
+    scala.util.Sorting.stableSort(sumCounts.toList,
+      (left: (String, Int), right: (String, Int)) => left._2 > right._2).take(10)
   }
 }
--- src/Main.scala
+++ src/Main.scala
@@ -2,6 +2,8 @@
   * Created by fonturacetamum on 04/09/16.
   */
 object Main extends App{
-  val stopWords = IndexBuilder.getStopWords(IndexBuilder.buildInvertedIndex(List("data/retina")), 10)
+//  val stopWords = IndexBuilder.getStopWords(IndexBuilder.buildInvertedIndex(List("data/retina")), 10)
+  val stopWords = IndexBuilder.getStopWords(IndexBuilder.buildInvertedIndex(List("data/doc1", "data/doc2")), 10)
   stopWords.foreach(println)
+//  println(IndexBuilder.buildInvertedIndex(List("data/doc1", "data/doc2")))
 }
