b7d85a4d583b3c2fe051581035aa367f95456eac
Artem Kondyukov <artemkondyukov@gmail.com>
2016-09-06 14:16:41 +0300

    Refactoring

--- src/QueryProcessor.scala
+++ src/QueryProcessor.scala
@@ -5,36 +5,15 @@ import scala.util.matching.Regex
   * Created by fonturacetamum on 04/09/16.
   */
 object QueryProcessor {
-  @tailrec final def intersect(p1: Seq[Int], p2: Seq[Int],
-                               isCloseEnough: (Int, Int) => Boolean,
-                               accumulator: Seq[Int] = Nil
-                              ): Seq[Int] = {
-
-    (p1, p2) match {
-      case (Nil, Nil) => accumulator
-      case (p, Nil) => accumulator
-      case (Nil, p) => accumulator
-      case (p1_head :: p1_tail, p2_head :: p2_tail) =>
-        if (isCloseEnough(p1_head, p2_head))
-          intersect(p1_tail, p2_tail, isCloseEnough, accumulator :+ p1_head)
-        else if (p1_head < p2_head)
-          intersect(p1_tail, p2, isCloseEnough, accumulator)
-        else
-          intersect(p1, p2_tail, isCloseEnough, accumulator)
-    }
-  }
-  def simpleIntersect = intersect(_: Seq[Int], _: Seq[Int], (l: Int, r: Int) => l == r)
-
   def preprocessQuery(query: String): String = {
     query.replace("*", ".*")
   }
 
   // Simple boolean queries using non-positional index
-  def evaluateBooleanQuery(query: String, index: Map[String, List[Int]]): List[Int] = {
+  def evaluateBooleanQuery(query: String, index: Map[String, List[(Int, String)]]): Set[(Int, String)] = {
     query.split(" ")
-      .map(word => index(word))
-      .reduce(simpleIntersect)
-      .toList
+      .map(word => index(word).toSet)
+      .reduce(_.intersect(_))
   }
 
   // A very dull implementation, linear in size of vocabulary
