84ef775bb243fe23610d858ec6184d2f612c2699
Artem Kondyukov <artemkondyukov@gmail.com>
2016-09-07 01:15:54 +0300

    main file added

--- src/Main.scala
+++ src/Main.scala
@@ -2,8 +2,25 @@
   * Created by fonturacetamum on 04/09/16.
   */
 object Main extends App{
-//  val stopWords = IndexBuilder.getStopWords(IndexBuilder.buildInvertedIndex(List("data/retina")), 10)
-  val stopWords = IndexBuilder.getStopWords(IndexBuilder.buildInvertedIndex(List("data/doc1", "data/doc2")), 10)
-  stopWords.foreach(println)
-//  println(IndexBuilder.buildInvertedIndex(List("data/doc1", "data/doc2")))
+  val index = IndexBuilder.buildInvertedIndex(List("data/doc1", "data/doc2", "data/doc3", "data/doc4", "data/doc5"))
+  val kGramIndex = IndexBuilder.buildKGramIndex(index.keys.toList)
+  val permutationIndex = IndexBuilder.buildPermutationIndex(index.keys.toList)
+
+  println()
+  println("KGram index: ")
+  kGramIndex foreach println
+
+  println()
+  println("Permutation index: ")
+  permutationIndex foreach println
+
+  println()
+  println("\"th* *d*a*y*\" query using kgrams: ")
+  val resultKGram = QueryProcessor.evaluateWildcardQueryKGram("th* *d*a*y*", index, kGramIndex)
+  resultKGram foreach println
+
+  println()
+  println("\"f*m d*y\" query using permutations: ")
+  val resultPermutations = QueryProcessor.evaluateWildcardQueryPermutation("f*m d*y", index, permutationIndex)
+  resultPermutations foreach println
 }
